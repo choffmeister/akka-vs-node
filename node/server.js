a455bef0c015aa4413e8cd3c215512a26e3f806e
Christian Hoffmeister <mail@choffmeister.de>
2015-01-19 19:09:01 +0100

    Add NodeJS project

---
+++ node/server.js
@@ -0,0 +1,18 @@
+var mongo = require('mongodb').MongoClient;
+
+mongo.connect('mongodb://localhost:27017/akkavsnode', function(err, db) {
+  var http = require('http');
+
+  http.createServer(function (req, res) {
+    if (req.url === '/api/user') {
+      var i = Math.ceil(Math.random() * 10000);
+      db.collection('users').findOne({ name: 'user-' + i }, function (err, user) {
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(user));
+      });
+    } else {
+      res.writeHead(404, {'Content-Type': 'application/json'});
+      res.end();
+    }
+  }).listen(8081, '0.0.0.0');
+});
