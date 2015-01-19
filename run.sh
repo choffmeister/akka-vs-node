b4a176383bf37801714f28a8120d309a44ed8cc1
Christian Hoffmeister <mail@choffmeister.de>
2015-01-19 20:04:32 +0100

    Improve process killing

--- run.sh
+++ run.sh
@@ -5,15 +5,17 @@ cat /dev/null > ./results.txt
 
 # start servers
 cd akka && ./target/pack/bin/server &
+akka_pid=$!
 cd node && node ./server.js &
-sleep 5
+node_pid=$!
 
 # warm up servers
+sleep 5
 wrk -c1 -t1 -d5 http://localhost:8080/api/user > /dev/null
 wrk -c1 -t1 -d5 http://localhost:8081/api/user > /dev/null
 sleep 5
 
-for i in 1 2 3 5 8 13 21 34 55 89 144; do
+for i in 1; do
   echo "Akka $i"
   wrk "-c$i" "-t$i" -d15 http://localhost:8080/api/user >> ./results.txt
   sleep 1
@@ -23,7 +25,8 @@ for i in 1 2 3 5 8 13 21 34 55 89 144; do
 done
 
 # stop servers
-killall java
-killall node
+sleep 5
+kill -9 $akka_pid
+kill -9 $node_pid
 
 exit 0
