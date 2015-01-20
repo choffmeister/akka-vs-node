d875f2a360e0c6a9a3ae28535dc0ab6a32f2d829
Christian Hoffmeister <mail@choffmeister.de>
2015-01-20 05:32:51 +0100

    Add more measurements

--- run.sh
+++ run.sh
@@ -15,7 +15,7 @@ wrk -c1 -t1 -d5 http://localhost:8080/api/user > /dev/null
 wrk -c1 -t1 -d5 http://localhost:8081/api/user > /dev/null
 sleep 5
 
-for i in 1; do
+for i in 1 2 3 5 8 13 21 34 55 89 144; do
   echo "Akka $i"
   wrk "-c$i" "-t$i" -d15 http://localhost:8080/api/user >> ./results.txt
   sleep 1
