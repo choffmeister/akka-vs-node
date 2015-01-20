53cf51e0c7671750985ee2b88ddf51905897d9c7
Christian Hoffmeister <mail@choffmeister.de>
2015-01-20 06:43:14 +0100

    Make node also response with pretty JSON

--- node/server.js
+++ node/server.js
@@ -8,7 +8,7 @@ mongo.connect('mongodb://localhost:27017/akkavsnode', function(err, db) {
       var i = Math.ceil(Math.random() * 10000);
       db.collection('users').findOne({ name: 'user-' + i }, function (err, user) {
         res.writeHead(200, {'Content-Type': 'application/json'});
-        res.end(JSON.stringify(user));
+        res.end(JSON.stringify(user, true, 2));
       });
     } else {
       res.writeHead(404, {'Content-Type': 'application/json'});
