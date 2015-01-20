0223bee385cc51c51d420174136e78a4fed35faf
Christian Hoffmeister <mail@choffmeister.de>
2015-01-20 06:43:25 +0100

    Change concurrency and runtime

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 WARMUP_DURATION="5"
-DURATION="15"
+DURATION="60"
 
 # clear results
 cat /dev/null > ./results.txt
@@ -19,7 +19,7 @@ wrk -c1 -t1 "-d$WARMUP_DURATION" http://localhost:8080/api/user >/dev/null
 wrk -c1 -t1 "-d$WARMUP_DURATION" http://localhost:8081/api/user >/dev/null
 sleep 1
 
-for CONCURRENCY in 1 2 3 5 8 13 21 34 55 89 144; do
+for CONCURRENCY in 1 2 4 8 16 32 64 128 256; do
   echo -e -n "akka\t$CONCURRENCY\t" >>./results.txt
   wrk --script ./report.lua "-c$CONCURRENCY" "-t$CONCURRENCY" "-d$DURATION" http://localhost:8080/api/user 2>>./results.txt
   sleep 1
